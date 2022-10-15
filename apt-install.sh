332900cf8500687a9698891f1e1eb293433ef91c
Sergey Vlasov <sergeyvlasov.pro@gmail.com>
2022-10-15 16:10:14 +0000

    separate setup logic into apt-install + dot-env

--- apt-install.sh
+++ apt-install.sh
@@ -3,12 +3,5 @@ sudo apt update
 sudo apt -y upgrade
 sudo apt install -y software-properties-common build-essential
 sudo apt install -y vim tree jq tmux
-
-# bash-it
-git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
-~/.bash_it/install.sh
-source ~/.bashrc
-
-# direnv
 sudo apt install -y direnv 
-[[ -f ~/.bash_it/custom/direnv.bash ]] && : || echo 'eval "$(direnv hook bash)"' >> ~/.bash_it/custom/direnv.bash 
+
